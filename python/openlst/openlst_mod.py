6c5ea7281baee9ad68cf2a98e15a84d42bfa1ee1
Ryan <theryanskabelund@gmail.com>
2024-04-03 01:26:01 -0700

    Transmitter functionality
    Added functionality for gnuradio to act as a transmitter.

--- python/openlst/openlst_mod.py
+++ python/openlst/openlst_mod.py
@@ -13,17 +13,13 @@ from .fec import encode_fec
 from .whitening import whiten
 from .crc import crc16
 
-class openlst_mod(gr.sync_block):
+class openlst_mod(gr.basic_block):
 	"""
 	OpenLST Encoder/Framer
 
 	This block encodes a raw data packet in the form:
 	
-		| HWID (2 bytes) | Seqnum (2 bytes) | Data (N bytes) |
-
-	aCtualLy it's THIS:
-		| Sequence bytes (2 bytes) | Length (1 byte) | HWID (2 bytes) |
-			Seqnum (2 bytes) | System byte (1 byte)  | Data (N bytes) |
+		| Sequence bytes (2 bytes) | Length (1 byte) | HWID (2 bytes) | Seqnum (2 bytes) | Data (N bytes) |
 	
 	To an RF message:
 
@@ -46,13 +42,15 @@ class openlst_mod(gr.sync_block):
 			sync_words=2,
 			flags=0xC0,
 			fec=True,
-			whitening=True
+			whitening=True,
+			seq_byte1=0x22,
+			seq_byte0=0x69
 		):
-		gr.sync_block.__init__(
+		gr.basic_block.__init__(
 			self,
 			name="OpenLST Encode and Frame",
 			in_sig=[np.uint8],
-			out_sig=[np.uint8],
+			out_sig=[np.uint8]
 		)
 
 		self.preamble_bytes = preamble_bytes
@@ -62,47 +60,90 @@ class openlst_mod(gr.sync_block):
 		self.flags = flags
 		self.fec = fec
 		self.whitening = whitening
+		self.sequence_bytes = [seq_byte1, seq_byte0]
 
-		self._buff = []
+		self._in_buffer = []
 		self._out_buffer = []
 
-	def handle_msg(self, msg):
-		raw = bytearray(msg)
+		self._input_items_offset = 0
+		self._to_consume = 0
 
-		# Insert the preamble and sync words
-		preamble = bytearray(
-			[0xaa] * self.preamble_bytes +  # preamble
-			[self.sync_byte1, self.sync_byte0] * self.sync_words)  # sync word(s)
-		
-		# Prefix with length byte and flags
-		content = bytes(
-			[len(raw) + 3] +  # length = raw + flags + checksum (2 bytes)
-			[self.flags]  # flags
-		)
-		content += raw[2:]  # data (includes seqnum)
-		# The HWID goes at the end for RF transmission
-		content += raw[0:2]
-		checksum = crc16(content)
+		self._mode = 'sequence-bytes'
+		self._length = None
 
-		# Append checksum
-		content += checksum.to_bytes(2, byteorder='little')
+		self._output_multiple = 10
+		self.set_output_multiple(self._output_multiple)
 
-		# Per the datasheet, whitening happens _before_ FEC
-		if self.whitening:
-			content = whiten(content)
-		if self.fec:
-			content = encode_fec(content)
+	def forecast(self, noutput_items, ninputs):
+		return [int(noutput_items/self._output_multiple) for i in range(ninputs)]
+	
+	def consume_input(self, num, pop_buffer=True, call_consume=True):
+		if pop_buffer:
+			self._in_buffer = self._in_buffer[num:]
+		if call_consume:
+			self.consume(0, num)
+			self._to_consume = max(0, self._to_consume - num)
+			self._input_items_offset = max(0, self._input_items_offset - num)
+		else:
+			self._to_consume += num
 
-		# Queue these bytes for transmission
-		self._out_buffer.append(preamble + content)
+	def general_work(self, input_items, output_items):
+		self._in_buffer.extend(input_items[0][self._input_items_offset:])
+		self._input_items_offset += len(input_items[0][self._input_items_offset:])
 
+		if self._mode == 'sequence-bytes':
+			while len(self._in_buffer) >= len(self.sequence_bytes):
+				sb = self._in_buffer[:2]
+				if sb == self.sequence_bytes:
+					print("Sequence bytes matched")
+					self._mode = 'length'
+					self.consume_input(2)
+					break
+				else:
+					self.consume_input(1)
+		elif self._mode == 'length':
+			if len(self._in_buffer) >= 1:
+				self._length = self._in_buffer[0]
+				print("Length matched:", self._length)
+				self._mode = 'data'
+				self.consume_input(1)
+		elif self._mode == 'data':
+			if len(self._in_buffer) >= self._length:
+				raw = bytes(self._in_buffer[:self._length])
 
-	def work(self, input_items, output_items):
-		# self._buff.extend(input_items[0])
+				# Insert the preamble and sync words
+				preamble = bytes(
+					[0xaa] * self.preamble_bytes +  # preamble
+					[self.sync_byte1, self.sync_byte0] * self.sync_words)  # sync word(s)
+				
+				# Prefix with length byte and flags
+				content = bytes(
+					[len(raw) + 3] +  # length = raw + flags + checksum (2 bytes)
+					[self.flags]  # flags
+				)
+				content += raw[2:]  # data (includes seqnum)
+				# The HWID goes at the end for RF transmission
+				content += raw[0:2]
+				checksum = crc16(content)
 
-		# Need a sample packet of data coming in, not sure if buffer is necessary or not.
-		# Main concern is knowing when previous command stops and the next one starts.
+				# Append checksum
+				content += checksum.to_bytes(2, byteorder='little')
 
+				# Per the datasheet, whitening happens _before_ FEC
+				if self.whitening:
+					content = whiten(content)
+				if self.fec:
+					content = encode_fec(content)
+
+				# Queue these bytes for transmission
+				encoded_data = [byte for byte in preamble + content]
+				self._out_buffer.extend(encoded_data)
+
+				self.consume_input(self._length, call_consume=False)
+				self._length = None
+				self._mode = "sequence-bytes"
+			else:
+				self.consume_input(len(input_items[0]), pop_buffer=False)
 
 		if len(self._out_buffer) > 0:
 			# Get sizes of out_buffer and output_items
@@ -123,13 +164,12 @@ class openlst_mod(gr.sync_block):
 			# Set out_buffer to new buffer
 			self._out_buffer = buff
 
-			# Consume unused bytes in the case that the number of output_items exceeds the data we have to send
-			if output_size > num_bytes_out:
-				self.consume(0, output_size - num_bytes_out)
+			# If at the end of sending the packet, consume all of the input for it
+			if (buffer_size <= output_size):
+				self.consume_input(self._to_consume, pop_buffer=False)
 
 			# Return number of bytes out
 			return num_bytes_out
 		
-		# Consume all input items and return 0 for no bytes to return
-		self.consume(0, len(input_items[0]))
+		# Return 0 for no bytes to return
 		return 0
